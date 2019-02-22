48d7f87a3d1264dcbee2b19608f93e47ef655292
DustinMiner <dustinjminer@outlook.com>
2019-02-21 20:25:00 -0600

    add inheritance

--- human.rb
+++ human.rb
@@ -1,19 +1,22 @@
-class Human
-  def name
-    @name
-  end
+class Mammal
+  attr_accessor :name, :age
 
-  def name=(name)
+  def initialize(name, age)
     @name = name
-  end
-
-  def age
-    @age
-  end 
-
-  def age=(age)
     @age = age
   end
+end
+
+
+class Human < Mammal
+  attr_accessor :hair_color, :best_friend
+ 
+ def initialize(name, age, hair_color)
+  super(name, age)
+  # @name = name
+  # @age = age
+  @hair_color = hair_color
+ end
 
     def speak
       p "Hello, my name is #{@name} and I am #{@age}."
@@ -21,14 +24,34 @@ class Human
     end
 
 
-dustin = Human.new
-dustin.name= "Dustin"
-dustin.age= 30
-puts dustin.name
-puts dustin.age
+
+# # todd.age= 25
+# todd.speak
+
+
+class Dog < Mammal
+  # attr_accessor :name, :age
+  # def initialize(name, age)
+  #   @name = name
+  #   @age = age
+  # end
+
+  def bark
+    puts "Bark, bark!"
+end
+end
+
+dustin = Human.new("dustin", 30, "Brown")
 dustin.speak
 
-todd = Human.new
-todd.name= "Todd"
-todd.age= 25
+
+todd = Human.new("Todd", 69, "Red")
 todd.speak
+
+spot = Dog.new("Spot", 12)
+# puts spot.name
+# puts spot.age
+# spot.bark
+
+dustin.best_friend = spot
+dustin.best_friend.bark
